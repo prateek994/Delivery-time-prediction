83a600f25ef19f2f8c04e0bd47f29c2e41acf06e
prateek994 <mauryaprateek8@gmail.com>
2024-03-19 11:49:00 +0530

    training

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ conda activate env/ -> CMD
 
 source activate env/ -> Git bash
 
-Install your requriments file
- pip install scikit-learn
+Install your requirements
+pip install -r requirements.txt
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -51,3 +53,8 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data , test_data = obj.initiate_data_ingestion()
 
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_training(train_arr, test_arr))
